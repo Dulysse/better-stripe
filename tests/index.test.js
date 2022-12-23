bb89c7ad2d51728e1dd1d749069ed5c26ffd39bf
Ulysse Dupont <ulysse@euranov.com>
2022-12-23 11:03:38 +0100

    unit testing

---
+++ tests/index.test.js
@@ -0,0 +1,15 @@
+const assert = require("assert").strict;
+const expect = require("chai").expect;
+require("dotenv").config();
+const { Stripe } = require("../");
+
+describe("Better Stripe", () => {
+	describe("Init", () => {
+		it("Init default instance", () => {
+			expect(Stripe.default).to.be.an("object");
+		});
+		it("Init a store instance", () => {
+			expect(Stripe.store(process.env.STRIPE_API_SECRET)).to.be.an("object");
+		});
+	});
+});
